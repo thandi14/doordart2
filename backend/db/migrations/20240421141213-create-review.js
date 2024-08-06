1febc5d41915b768aa0fc385fdba57faf5311092
thandi mpofu <thandimpofu2003@gmail.com>
2024-08-06 09:01:34 -0500

    deploy

--- backend/db/migrations/20240421141213-create-review.js
+++ backend/db/migrations/20240421141213-create-review.js
@@ -57,7 +57,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Reviews";
