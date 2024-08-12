689a01e4c08db234bc592ecdebd76071c8b8f7bb
thandi mpofu <thandimpofu2003@gmail.com>
2024-08-12 09:17:02 -0500

    recent searchs for db

---
+++ backend/db/migrations/20240812140856-create-search.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Searches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+      },
+      onDelete: 'cascade'
+      },
+      query: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Searches";
+    return queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/seeders/20240812141102-searches.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { Search } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Search.bulkCreate([
+      {
+        userId: 1,
+        query: "food",
+      },
+      {
+        userId: 1,
+        query: "restaurant",
+      },
+      {
+        userId: 1,
+        query: "breakfast",
+      },
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Searches';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, null, {})
+  }
+};
